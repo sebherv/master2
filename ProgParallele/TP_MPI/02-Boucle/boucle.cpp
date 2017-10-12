d54ec14347267cc469a5df3a3baa0fdd160cec24
Sebastien Hervieu <shervieu@id-apps.fr>
2017-10-12 11:54:32 +0200

    Avec les données de temps

--- ProgParallele/TP_MPI/02-Boucle/boucle.cpp
+++ ProgParallele/TP_MPI/02-Boucle/boucle.cpp
@@ -21,7 +21,9 @@ int main(int argc, char *argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	int inputInt = 0;
+	int initRingNum = 0;
 	int ringNum = 0;
+	int printMsg = 1; // Print by default
 
 	if(rank == MASTER_RANK) {
 
@@ -36,6 +38,7 @@ int main(int argc, char *argv[])
 			if(strcmp("numRings", argv[i]) == 0) {
 				sscanf(argv[i+1], "%d", &ringNum);
 			}
+			printMsg = 0;
 
 		}
 
@@ -50,8 +53,16 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	double t0 = MPI_Wtime();
+
 	// Broadcast the number of times the ring must go round
 	MPI_Bcast(&ringNum,1,MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+	initRingNum = ringNum;
+
+	// Broadcast weither we print messages
+	MPI_Bcast(&printMsg, 1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+
+	MPI_Barrier(MPI_COMM_WORLD);
 
 
 	if(rank == MASTER_RANK) {
@@ -80,11 +91,13 @@ int main(int argc, char *argv[])
 		MPI_Recv(recvBuf, count, MPI_FLOAT, MPI_ANY_SOURCE, TAG_RING, MPI_COMM_WORLD, &status);
 		int sender = status.MPI_SOURCE;
 
-		cout << "Rank " << rank << " has received the following floats ";
-		for(int i = 0 ; i < count ; i++) {
-			cout << " " << recvBuf[i];
+		if(printMsg != 0) {
+			cout << "Rank " << rank << " has received the following floats ";
+			for(int i = 0 ; i < count ; i++) {
+				cout << " " << recvBuf[i];
+			}
+			cout << " from rank " << sender << endl;	
 		}
-		cout << " from rank " << sender << endl;
 	
 		int nextRank;
 		if(rank == worldSize - 1) {
@@ -95,6 +108,13 @@ int main(int argc, char *argv[])
 	
 		MPI_Send(recvBuf, count, MPI_FLOAT, nextRank, TAG_RING, MPI_COMM_WORLD);
 	
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	double t1 = MPI_Wtime();
+
+	if(rank == MASTER_RANK) {
+		cout << inputInt << " " << ringNum << " " << t1 -t0 << endl; 
 	}
 
 	MPI_Finalize();
